dc2903fc2fe9e3222159df9275fd04586c978b5f
Manuel Sánchez <msanchezmoya05@gmail.com>
2024-11-04 10:17:18 +0100

    Ejer5

---
+++ Ejer1_05_Js/funciones.js
@@ -0,0 +1,23 @@
+/**
+         * Funcion para calcular la cantidad
+         * @p int - cantidad de personas
+         * @c int - numeoro de calorias totales
+         * @return int - numero de kilos necesarios
+         */
+function calculaCantidad(p, c) {
+
+}
+
+
+
+/**
+ * 
+ * @param {*} personas 
+ * @param {*} calorias 
+ */
+function calculaCantidadGrupo(personas, calorias){
+
+}
+
+// llamada a la funcion
+calculaCantidad(4, 6000); //
